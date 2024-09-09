cc90ef187bb61f8a05c548a69c71eb928e8bfc58
samueltobiasrolanduyet <samueltobiasrolanduyet@gmail.com>
2024-09-09 10:20:07 +0200

    navbar added

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../assets/logo.webp';
+import { HStack, Image, Text } from '@chakra-ui/react';
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image
+        src={logo}
+        alt='logo'
+        boxSize={'60px'}
+      />
+      <Text> Navbar</Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
