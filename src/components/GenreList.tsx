31899b2d00e0e2b4fab30410c8585de3b257bebc
Tobias Roland Uyet <samu1493@stud.kea.dk>
2024-09-18 08:05:16 +0200

    added spiner when genres os loading

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -2,30 +2,36 @@ import React from 'react';
 import useGenres from '../hooks/useGenres';
 import { Heading, HStack, Image, List, ListItem } from '@chakra-ui/react';
 import { getCroppedImageUrl } from '../services/image-urls';
-
+import { Spinner } from '@chakra-ui/react';
 const GenreList = () => {
   const { data: genres, error, isLoading } = useGenres();
 
   return (
     <List spacing={4}>
-      {genres.map((genre) => (
-        <ListItem key={genre.id}>
-          <HStack gap={4}>
-            <Image
-              boxSize={8}
-              objectFit={'cover'}
-              src={getCroppedImageUrl(genre.image_background)}
-              alt={genre.name}
-            />
-            <Heading
-              as={'h4'}
-              size={'sm'}
-            >
-              {genre.name}
-            </Heading>
-          </HStack>
-        </ListItem>
-      ))}
+      {isLoading
+        ? Array.from({ length: 20 }).map((_, index) => (
+            <ListItem>
+              <Spinner />
+            </ListItem>
+          ))
+        : genres?.map((genre) => (
+            <ListItem key={genre.id}>
+              <HStack gap={4}>
+                <Image
+                  boxSize={8}
+                  objectFit={'cover'}
+                  src={getCroppedImageUrl(genre.image_background)}
+                  alt={genre.name}
+                />
+                <Heading
+                  as={'h4'}
+                  size={'sm'}
+                >
+                  {genre.name}
+                </Heading>
+              </HStack>
+            </ListItem>
+          ))}
     </List>
   );
 };
