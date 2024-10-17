cb2c93d37ee4bd5f1371cbbf62ac21a841126910
Tobias Roland Uyet <samu1493@stud.kea.dk>
2024-10-17 08:15:56 +0200

    Refactor import path for init module and update database port in ormconfig.json

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
-import init from './startup/init';
+import init from './start/init';
 
 const app = express();
 app.use(cors());
